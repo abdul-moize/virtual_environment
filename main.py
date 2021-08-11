c0331b1a8e8c18b87a3a2151badba02b1564ee7f
Abdul-Moize <abdul.moize@arbisoft.com>
2021-08-11 18:18:21 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+url_posts = 'https://jsonplaceholder.typicode.com/posts'
+posts = requests.get(url_posts).json()
+url_users = 'https://jsonplaceholder.typicode.com/users'
+count = 0
+user_id = posts[0]['userId']
+for i in posts:
+    if i['userId'] == user_id:
+        count += 1
+del posts
+users = requests.get(url_users).json()
+print('Number of Posts by user: ' + users[user_id-1]['username'] + ' is ' + str(count))
