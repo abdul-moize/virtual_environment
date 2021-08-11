c14a905f3b5b3084734e8b0053e19db4017d9d75
Abdul-Moize <abdul.moize@arbisoft.com>
2021-08-11 19:01:44 +0500

    commit 2.0

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import numpy
+import pandas
+import plotext as plt
 
+# requests library exercise
 url_posts = 'https://jsonplaceholder.typicode.com/posts'
 posts = requests.get(url_posts).json()
 url_users = 'https://jsonplaceholder.typicode.com/users'
@@ -9,13 +12,22 @@ user_id = posts[0]['userId']
 for i in posts:
     if i['userId'] == user_id:
         count += 1
+# delete posts json array to save space
 del posts
 users = requests.get(url_users).json()
 print('Number of Posts by user: ' + users[user_id-1]['username'] + ' is ' + str(count))
 
+# numpy library exercise
 numbers = numpy.arange(10)
 # change the even values to 0
 numbers[numbers % 2 == 0] = 0
 print(numbers)
 
+# plotext exercise
+y = plt.sin(100, 3)
+plt.plot(y)
+plt.plotsize(100, 30)
+plt.title("Plot Example")
+plt.show()
 
+# pandas exercise
